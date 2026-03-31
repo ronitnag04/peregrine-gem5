cb161bb9590b64e5697fad3d17c75cc9a3fcc720
Ronit Nagarapu <ronitnag04@berkeley.edu>
2026-03-31 05:02:18 +0000

    configs: Added SPEC bmarks, --max-insts & --fast-forward options

--- configs/peregrine/peregrine.py
+++ configs/peregrine/peregrine.py
@@ -1,13 +1,16 @@
 import argparse
+from pathlib import Path
 from typing import (
     Callable,
     Optional,
     Type,
 )
 
+import m5
 from m5.objects import (
     TAGE,
     X86O3CPU,
+    AtomicSimpleCPU,
     BasePrefetcher,
     BaseXBar,
     BranchPredictor,
@@ -23,6 +26,7 @@ from m5.objects.FuncUnit import *
 from m5.objects.FuncUnitConfig import *
 
 from gem5.components.boards.abstract_board import AbstractBoard
+from gem5.components.boards.mem_mode import MemMode
 from gem5.components.boards.simple_board import SimpleBoard
 from gem5.components.cachehierarchies.abstract_cache_hierarchy import (
     AbstractCacheHierarchy,
@@ -36,8 +40,10 @@ from gem5.components.cachehierarchies.classic.private_l1_shared_l2_cache_hierarc
 from gem5.components.memory.single_channel import SingleChannelDDR4_2400
 from gem5.components.processors.base_cpu_core import BaseCPUCore
 from gem5.components.processors.base_cpu_processor import BaseCPUProcessor
+from gem5.components.processors.switchable_processor import SwitchableProcessor
 from gem5.isas import ISA
 from gem5.resources.resource import BinaryResource
+from gem5.simulate.exit_event import ExitEvent
 from gem5.simulate.simulator import Simulator
 from gem5.utils.override import overrides
 
@@ -92,10 +98,22 @@ def parse_args():
             "sparse",
             "towers",
             "whetstone",
+            "505.mcf_r",
+            "520.omnetpp_r",
+            "523.xalancbmk_r",
+            "541.leela_r",
+            "548.exchange2_r",
+            "531.deepsjeng_r",
+            "557.xz_r",
+            # "500.perlbench_r",
+            "525.x264_r",
+            "502.gcc_r",
         ],
     )
-    # Tracing
+    # Execution behavior
     parser.add_argument("--trace", action="store_true", default=False)
+    parser.add_argument("--max-insts", type=int)
+    parser.add_argument("--fast-forward", type=int)
     # Output directory
     parser.add_argument("--outdir", type=str, default="m5out")
     return parser.parse_args()
@@ -140,8 +158,9 @@ class MyOutOfOrderCore(BaseCPUCore):
         lq_entries,
         sq_entries,
         branch_predictor,
+        core_id: int = 0,
     ):
-        super().__init__(X86O3CPU(), ISA.X86)
+        super().__init__(X86O3CPU(cpu_id=core_id), ISA.X86)
         # TODO: Convert all parameter settings to use Param notation
         self.core.numROBEntries = rob_size
         self.core.fetchWidth = fetch_width
@@ -159,35 +178,40 @@ class MyOutOfOrderCore(BaseCPUCore):
         self.core.branchPred = branch_predictor
 
 
+class AtomicCore(BaseCPUCore):
+    def __init__(self, core_id: int = 0):
+        super().__init__(AtomicSimpleCPU(cpu_id=core_id), ISA.X86)
+
+
 class MyOutOfOrderProcessor(BaseCPUProcessor):
-    def __init__(
-        self,
-        fetch_width,
-        decode_width,
-        rename_width,
-        wb_width,
-        commit_width,
-        issue_width,
-        rob_size,
-        lq_entries,
-        sq_entries,
-        branch_predictor,
-    ):
-        cores = [
-            MyOutOfOrderCore(
-                fetch_width,
-                decode_width,
-                rename_width,
-                wb_width,
-                commit_width,
-                issue_width,
-                rob_size,
-                lq_entries,
-                sq_entries,
-                branch_predictor,
-            )
-        ]
-        super().__init__(cores)
+    def __init__(self, core: MyOutOfOrderCore):
+        super().__init__([core])
+
+
+class FastForwardToO3Processor(SwitchableProcessor):
+    def __init__(self, detailed_core: MyOutOfOrderCore, core_id: int = 0):
+        self._start_key = "fast_forward"
+        self._switch_key = "detailed"
+        self._is_fast_forward = True
+        switchable_cores = {
+            self._start_key: [AtomicCore(core_id=core_id)],
+            self._switch_key: [detailed_core],
+        }
+        super().__init__(
+            switchable_cores=switchable_cores,
+            starting_cores=self._start_key,
+        )
+
+    @overrides(SwitchableProcessor)
+    def incorporate_processor(self, board: AbstractBoard) -> None:
+        super().incorporate_processor(board=board)
+        board.set_mem_mode(MemMode.ATOMIC)
+
+    def switch(self):
+        if self._is_fast_forward:
+            self._board.set_mem_mode(MemMode.TIMING)
+            self.switch_to_processor(self._switch_key)
+            self._is_fast_forward = False
 
 
 class MyCacheHierarchy(PrivateL1SharedL2CacheHierarchy):
@@ -272,7 +296,7 @@ elif _args.branch_predictor == "tage":
 else:
     raise ValueError(f"Invalid branch predictor: {_args.branch_predictor}")
 
-processor = MyOutOfOrderProcessor(
+detailed_core = MyOutOfOrderCore(
     fetch_width=_args.fetch_width,
     decode_width=_args.decode_width,
     rename_width=_args.rename_width,
@@ -283,16 +307,30 @@ processor = MyOutOfOrderProcessor(
     lq_entries=_args.lq_entries,
     sq_entries=_args.sq_entries,
     branch_predictor=branch_predictor,
+    core_id=0,
 )
 
+if _args.fast_forward:
+    processor = FastForwardToO3Processor(
+        detailed_core=detailed_core, core_id=0
+    )
+    ff_cpu = processor.fast_forward[0].get_simobject()
+    ff_cpu.max_insts_any_thread = _args.fast_forward
+    detailed_cpu = detailed_core.get_simobject()
+else:
+    processor = MyOutOfOrderProcessor(core=detailed_core)
+    detailed_cpu = processor.get_cores()[0].get_simobject()
+
 if _args.trace:
-    cpu_core = processor.get_cores()[0]
-    actual_cpu = cpu_core.get_simobject()
     tracer = InstructionTracer(
-        manager=actual_cpu,
+        manager=detailed_cpu,
         output_file="trace.csv",
     )
-    actual_cpu.instruction_tracer = tracer
+    detailed_cpu.instruction_tracer = tracer
+
+if _args.max_insts:
+    print(f"Setting max instructions of detailed CPU to {_args.max_insts}")
+    detailed_cpu.max_insts_any_thread = _args.max_insts
 
 main_memory = SingleChannelDDR4_2400(size="4GiB")
 cache_hierarchy = MyCacheHierarchy(
@@ -313,10 +351,143 @@ board = SimpleBoard(
     clk_freq="3GHz",
 )
 
-binary = BinaryResource(
-    local_path=f"tests/peregrine-bmarks/{_args.benchmark}-gem5"
-)
-board.set_se_binary_workload(binary)
+peregrine_benchmarks = [
+    "branch_storm",
+    "collatz",
+    "dhrystone",
+    "linpack",
+    "sieve",
+    "sparse",
+    "towers",
+    "whetstone",
+]
 
-simulator = Simulator(board=board, outdir=f"{_args.outdir}")
+
+def file_arg(benchmark_name, filename):
+    return f"tests/peregrine-spec/{benchmark_name}/{filename}"
+
+
+spec_benchmarks = {
+    "505.mcf_r": {
+        "binary": "mcf_r_base.peregrine-m64",
+        "arguments": [file_arg("505.mcf_r", "inp.in")],
+    },
+    "520.omnetpp_r": {
+        "binary": "omnetpp_r_base.peregrine-m64",
+        "arguments": [
+            "-f",
+            file_arg("520.omnetpp_r", "omnetpp.ini"),
+            "-c",
+            "General",
+            "-r",
+            "0",
+        ],
+    },
+    "523.xalancbmk_r": {
+        "binary": "cpuxalan_r_base.peregrine-m64",
+        "arguments": [
+            "-v",
+            file_arg("523.xalancbmk_r", "test.xml"),
+            file_arg("523.xalancbmk_r", "xalanc.xsl"),
+        ],
+    },
+    "541.leela_r": {
+        "binary": "leela_r_base.peregrine-m64",
+        "arguments": [file_arg("541.leela_r", "test.sgf")],
+    },
+    "548.exchange2_r": {
+        "binary": "exchange2_r_base.peregrine-m64",
+        "arguments": ["0"],
+    },
+    "531.deepsjeng_r": {
+        "binary": "deepsjeng_r_base.peregrine-m64",
+        "arguments": [file_arg("531.deepsjeng_r", "test.txt")],
+    },
+    "557.xz_r": {
+        "binary": "xz_r_base.peregrine-m64",
+        "arguments": [
+            file_arg("557.xz_r", "cpu2006docs.tar.xz"),
+            "4",
+            "055ce243071129412e9dd0b3b69a21654033a9b723d874b2015c774fac1553d9"
+            "713be561ca86f74e4f16f22e664fc17a79f30caa5ad2c04fbc447549c2810fae",
+            "1548636",
+            "1555348" "0",
+        ],
+    },
+    # TODO: perlbench not working outside of directory
+    # "500.perlbench_r": {
+    #     "binary": "perlbench_r_base.peregrine-m64",
+    #     "arguments": [file_arg("500.perlbench_r", "test.pl")]},
+    "525.x264_r": {
+        "binary": "x264_r_base.peregrine-m64",
+        "arguments": [
+            "--dumpyuv",
+            "50",
+            "--frames",
+            "156",
+            "-o",
+            file_arg("525.x264_r", "BuckBunny_New.264"),
+            file_arg("525.x264_r", "BuckBunny.yuv"),
+            "1280x720",
+        ],
+    },
+    "502.gcc_r": {
+        "binary": "cpugcc_r_base.peregrine-m64",
+        "arguments": [
+            file_arg("502.gcc_r", "t1.c"),
+            "-O3",
+            "finline-limit=50000",
+            "-o",
+            file_arg("502.gcc_r", "t1.opts-O3_-finline-limit_50000.s"),
+        ],
+    },
+}
+
+if _args.benchmark in peregrine_benchmarks:
+    binary = BinaryResource(
+        local_path=f"tests/peregrine-bmarks/{_args.benchmark}-gem5"
+    )
+    arguments = []
+elif _args.benchmark in spec_benchmarks:
+    binary = spec_benchmarks[_args.benchmark]["binary"]
+    arguments = spec_benchmarks[_args.benchmark]["arguments"]
+    binary = BinaryResource(
+        local_path=f"tests/peregrine-spec/{_args.benchmark}/{binary}"
+    )
+else:
+    raise ValueError(f"Invalid benchmark: {_args.benchmark}")
+
+board.set_se_binary_workload(
+    binary=binary,
+    arguments=arguments,
+    stdout_file=Path("stdout.txt"),
+    stderr_file=Path("stderr.txt"),
+)
+
+if _args.fast_forward:
+
+    def _switch_after_fast_forward():
+        print(
+            f"Fast-forward complete at {_args.fast_forward} instructions; "
+            "switching to detailed O3 CPU."
+        )
+        if _args.max_insts:
+            print(f"Simulating O3 CPU with max insts: {_args.max_insts}")
+        processor.switch()
+        m5.stats.reset()
+        # Continue simulation after the switch.
+        yield False
+        # For any later MAX_INSTS events (e.g., --max-insts on O3), exit.
+        yield True
+
+    print(
+        f"Fast-forwarding to {_args.fast_forward} instructions using AtomicSimpleCPU"
+    )
+    simulator = Simulator(
+        board=board,
+        outdir=f"{_args.outdir}",
+        on_exit_event={ExitEvent.MAX_INSTS: _switch_after_fast_forward()},
+    )
+else:
+    simulator = Simulator(board=board, outdir=f"{_args.outdir}")
 simulator.run()
